563946356a1fe74f63ae3985398fe379bee0be49
Justice Gong <justice.gong@hotmail.com>
2017-05-05 17:32:02 -0400

    Data trim

---
+++ Recommend System/DataTrim.py
@@ -0,0 +1,23 @@
+import json
+
+with open('D:/Workplace/BigData/yelp_academic_dataset_review_trim.json', 'w', encoding="utf8") as dest_file:
+    with open('D:/Workplace/BigData/yelp_academic_dataset_review - Copy.json', 'r', encoding="utf8") as source_file:
+        for line in source_file:
+            element = json.loads(line.strip())
+            if 'date' in element:
+                del element['date']
+            if 'text' in element:
+                del element['text']
+            if 'useful' in element:
+                del element['useful']
+            if 'funny' in element:
+                del element['funny']
+            if 'cool' in element:
+                del element['cool']
+            if 'type' in element:
+                del element['type']
+            dest_file.write(json.dumps(element))
+
+
+
+
